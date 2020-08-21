6182a0cdd6e13e72b670e4529ee3e7673252d4d4
HurtadoAndres <33639914+HurtadoAndres@users.noreply.github.com>
2020-08-21 10:26:12 -0500

    Update ejercicio7.py

--- Ejercicio_taller/ejercicio7/ejercicio7.py
+++ Ejercicio_taller/ejercicio7/ejercicio7.py
@@ -7,7 +7,7 @@ if n1 < n2:
         if resul%2 == 0:
             print(f"Es par {resul} ")
         else:
-            print(f" Es impar {resul} ")
+            print(f"Es impar {resul} ")
 
 else:
     print("El numero 1 debe ser mayor que el numero 2")
